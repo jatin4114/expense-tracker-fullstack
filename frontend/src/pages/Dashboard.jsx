28aa3a148e2ed08761b0e7cbc3b066914a120558
Jatin Panghal <jatinpanghal007@gmail.com>
2022-04-22 16:40:00 +0530

    feat: display expenses on dashboard
    dashboard now fetches real expenses from the api instead of using
    sample data - total spending, this month spending, remaining budget
    and the recent expenses / chart all reflect live data. added loading
    and error states. budget amount is still a placeholder until phase 8.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,36 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { getExpenses } from "../api/expenses";
 import SummaryCard from "../components/dashboard/SummaryCard";
 import RecentExpenses from "../components/dashboard/RecentExpenses";
 import SpendingChart from "../components/dashboard/SpendingChart";
-import { sampleExpenses, sampleBudget } from "../utils/sampleData";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import ErrorMessage from "../components/common/ErrorMessage";
+import { sampleBudget } from "../utils/sampleData";
 import "./Dashboard.css";
 
 function Dashboard() {
-  // TODO phase 3/4: replace sample data with a real api call
-  const expenses = sampleExpenses;
+  const [expenses, setExpenses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // TODO phase 8: replace sampleBudget with the real budget from the api
   const budget = sampleBudget;
+
+  useEffect(() => {
+    loadExpenses();
+  }, []);
+
+  function loadExpenses() {
+    setLoading(true);
+    setError(null);
+    getExpenses()
+      .then((data) => setExpenses(data))
+      .catch((err) => {
+        console.error(err);
+        setError("Could not load your expenses. Is the backend running?");
+      })
+      .finally(() => setLoading(false));
+  }
 
   // figuring out this month's expenses vs everything
   const thisMonthExpenses = useMemo(() => {
@@ -24,6 +46,14 @@ function Dashboard() {
   const totalSpending = expenses.reduce((sum, e) => sum + e.amount, 0);
   const monthSpending = thisMonthExpenses.reduce((sum, e) => sum + e.amount, 0);
   const remainingBudget = budget.monthly_limit - monthSpending;
+
+  if (loading) {
+    return <LoadingSpinner text="Loading your dashboard..." />;
+  }
+
+  if (error) {
+    return <ErrorMessage message={error} onRetry={loadExpenses} />;
+  }
 
   return (
     <div>
